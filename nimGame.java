376f3fda71d1c25000e3d5bd1c3f2dd1d9124b0f
systems32 <ryanandlin@gmail.com>
2023-01-19 19:24:43 -0800

    started work on game class

--- nimGame.java
+++ nimGame.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class nimGame
 {
     public String game()
     {
+        boolean tracker = true; 
+        nimBoard nim = new nimBoard();
+        Scanner myObj = new Scanner(System.in);
+        int number = 0;
+        
+
+
+        while (tracker == true)
+        {
+            System.out.println(nim.getCard() + " cards remain");
+            if (number % 2 == 0)
+            {
+                System.out.println("Player 1 please eneter in your number");
+            }
+            else {
+                System.out.println("Player 2 please eneter in your number");
+            }
+
+            String userInput = myObj.nextLine();
+        }
         return "test";
     }
 }
